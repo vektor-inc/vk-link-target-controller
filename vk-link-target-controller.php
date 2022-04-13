0cd3145cb63353a25d7fc338440da32f066f809b
kurudrive <kurudrive@gmail.com>
2022-04-13 17:14:11 +0900

    [ Change version ] 1.6.5

--- vk-link-target-controller.php
+++ vk-link-target-controller.php
@@ -3,7 +3,7 @@
 Plugin Name: VK Link Target Controller
 Plugin URI: https://github.com/kurudrive/vk-link-target-controller
 Description: Allow you to link a post title from the recent posts list to another page (internal or external link) rather than link to the actual post page
-Version: 1.6.4
+Version: 1.6.5
 Author: Vektor,Inc.
 Author URI: http://www.vektor-inc.co.jp/
 License: GPL2
@@ -541,7 +541,7 @@ jQuery(document).ready(function($){
 			$post_types         = $this->get_public_post_types(); // array of post types to create a checkbox list
 			$post_types['page'] = __( 'Pages' );
 			$post_types_slugs   = array_keys( $post_types );
-			return get_option( 'custom-post-types', $post_types_slugs );		
+			return get_option( 'custom-post-types', $post_types_slugs );
 		}
 
 		/**
